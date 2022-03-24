64eb4548305826d2faae5de6aff71acc1df18382
obboi <102006885+obboi@users.noreply.github.com>
2022-03-24 11:16:08 +0700

    repair folder

--- 3. CoreModulesNPM/app.js
+++ 3. CoreModulesNPM/app.js
@@ -15,16 +15,20 @@ rl.question(`What is your name? `, (name)=>
         {
             rl.question(`This your email? `, (email)=>
             {
+            validator.isEmail(email)    
             if(validator.isEmail(email) == true){
-            console.log(`Terimakasih ${name}, Nomor telephone kamu ${mobile}, Dengan email ${email}`);
+            console.log(`Terimakasih ${name},Nomor telephone kamu ${mobile}`, `Dengan email ${email}`);
             rl.close();
             process.exit();
             }else {
-                console.log('Maaf email yang anda masukan salah');
+                console.log(`Maaf email yang anda masukan salah`)
             }
             });
         }else {
-            console.log('Maaf nomor anda tidak terdaftar');
+            rl.question(`your mobile number `, (mobile)=>
+            {
+               validator.isMobilePhone(mobile);
+            });
         }
 
 
---
+++ 3. CoreModulesNPM/loop.js
@@ -0,0 +1,8 @@
+const validator = require('validator');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+
