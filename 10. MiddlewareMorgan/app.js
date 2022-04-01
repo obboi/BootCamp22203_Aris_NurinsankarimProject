0684d867fdbedefe1f5551bc580b89e41dd2d645
obboi <102006885+obboi@users.noreply.github.com>
2022-04-01 13:51:37 +0700

    Middleware beserta Morgan-smash :D

---
+++ 10. MiddlewareMorgan/app.js
@@ -0,0 +1,77 @@
+// Express-EJS-Layout
+
+const express = require('express');
+const { request } = require('http');
+const expressLayouts = require('express-ejs-layouts')
+const morgan  = require('morgan')
+const app = express()
+const path = require('path');
+const port = 3000
+
+
+
+// morgan
+app.use(morgan('dev'));
+
+// information ejs
+app.use(expressLayouts)
+app.set('layout', './layout/gabung')
+app.set('view engine','ejs')
+
+app.use(express.static(__dirname + '/public'));    
+
+// time digunakan untuk menghitung detik dari tanggal 1 januari 1970
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  next()
+})
+
+app.get('/', (req, res) => {
+  cont =[
+    {
+    nama:'Budi',
+    email:'bdi@gmail.com',
+  },{
+    nama:'andi',
+    email:'and@gmail.com',
+  },{
+    nama:'audi',
+    email:'aui@gmail.com',
+  },
+  ]
+
+  // memasukan function agar dapat memanggil setiap function yg terdapat pada file .ejs
+    const nama ="Aris"
+    const title ="Wellcome to my page"
+    res.render('index',
+    {
+      nama,
+      title,
+      cont,
+    })
+   
+});
+
+app.get('/about', (req, res) => {
+    const title ="about page"
+    res.render('about',{title})
+});
+
+app.get('/contact', (req, res) => {
+  const title ="contact page"
+  res.render('contact',{title})
+  
+})
+
+     
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('Page Not Found 404')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
